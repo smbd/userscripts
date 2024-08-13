1c57c87fc7fc58721bb22b4a7203dda888023a47
Mitsuru SHIMAMURA <smbd.jp@gmail.com>
2024-08-13 18:47:20 +0900

    use jQuery free waitForKeyElements.js

--- X-title-cleaner.user.js
+++ X-title-cleaner.user.js
@@ -2,13 +2,12 @@
 // @name         X title cleaner
 // @description  X title cleaner
 // @namespace    http://smbd.jp/
-// @version      2024-08-13-01
+// @version      2024-08-13-02
 // @author       smbd
 // @match        https://x.com/*/status/*
 // @match        https://twitter.com/*/status/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
-// @require      https://code.jquery.com/jquery-3.7.1.slim.min.js
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://raw.githubusercontent.com/smbd/userscripts/main/waitForKeyElements.js
 // @updateURL    https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
 // @downloadURL  https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
 // ==/UserScript==
