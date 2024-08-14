a657043be383faf67c7d5084073dea9577a75723
Mitsuru SHIMAMURA <smbd.jp@gmail.com>
2024-08-14 21:02:24 +0900

    setIntervalを使う方式に変更

--- X-title-cleaner.user.js
+++ X-title-cleaner.user.js
@@ -2,17 +2,20 @@
 // @name         X title cleaner
 // @description  X title cleaner
 // @namespace    http://smbd.jp/
-// @version      2024-08-13-02
+// @version      2024-08-14-01
 // @author       smbd
 // @match        https://x.com/*/status/*
 // @match        https://twitter.com/*/status/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
-// @require      https://raw.githubusercontent.com/smbd/userscripts/main/waitForKeyElements.js
 // @updateURL    https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
 // @downloadURL  https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
 // ==/UserScript==
 
-waitForKeyElements('div.public-DraftEditorPlaceholder-inner', function() {
-    const match = document.title.match(/^(?:\(\d+\) )?Xユーザーの(.*)さん: (.*)\s+\/ X/i);
-    document.title = `${match[1]} ${match[2]}`;
-});
+const intervalID = setInterval(function () {
+    const match = document.title.match(/^(?:\(\d+\) )?Xユーザーの(.*)さん: 「(.*)」\s+\/ X/i);
+    if (match) {
+        const userid = document.querySelector('div[data-testid="User-Name"] > div:nth-child(2) > div > div > a > div > span').innerText;
+        document.title = `${match[1]} (${userid}): ${match[2]}`;
+        clearInterval(intervalID);
+    }
+}, 1000);
