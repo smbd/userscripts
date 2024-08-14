ece3a2b62117919db458ebcc734b57b424841679
Mitsuru SHIMAMURA <smbd.jp@gmail.com>
2024-08-14 21:22:53 +0900

    @match が個々のページにマッチしないときがあるのでx.com全体で動作するように
    ホームTL/プロフページから個々のポストに遷移すると、@matchに/status/を入れているとマッチしない

--- X-title-cleaner.user.js
+++ X-title-cleaner.user.js
@@ -4,8 +4,8 @@
 // @namespace    http://smbd.jp/
 // @version      2024-08-14-01
 // @author       smbd
-// @match        https://x.com/*/status/*
-// @match        https://twitter.com/*/status/*
+// @match        https://x.com/*
+// @match        https://twitter.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
 // @updateURL    https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
 // @downloadURL  https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
@@ -13,9 +13,9 @@
 
 const intervalID = setInterval(function () {
     const match = document.title.match(/^(?:\(\d+\) )?Xユーザーの(.*)さん: 「(.*)」\s+\/ X/i);
-    if (match) {
-        const userid = document.querySelector('div[data-testid="User-Name"] > div:nth-child(2) > div > div > a > div > span').innerText;
-        document.title = `${match[1]} (${userid}): ${match[2]}`;
-        clearInterval(intervalID);
+    if ( document.URL.match(/\/status\//) && match ) {
+            const userid = document.querySelector('div[data-testid="User-Name"] > div:nth-child(2) > div > div > a > div > span').innerText;
+            document.title = `${match[1]} (${userid}): ${match[2]}`;
+            clearInterval(intervalID);
     }
 }, 1000);
