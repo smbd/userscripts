a82beadd89d72fb144f29e447debd4c4d10f435c
Mitsuru SHIMAMURA <smbd.jp@gmail.com>
2026-04-20 20:44:56 +0900

    fix user ID query

--- X-title-cleaner.user.js
+++ X-title-cleaner.user.js
@@ -2,10 +2,9 @@
 // @name         X title cleaner
 // @description  X title cleaner
 // @namespace    http://smbd.jp/
-// @version      2024-08-14-01
+// @version      1.1.0
 // @author       smbd
 // @match        https://x.com/*
-// @match        https://twitter.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
 // @updateURL    https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
 // @downloadURL  https://github.com/smbd/userscripts/raw/main/X-title-cleaner.user.js
@@ -14,8 +13,11 @@
 const intervalID = setInterval(function () {
     const match = document.title.match(/^(?:\(\d+\) )?Xユーザーの(.*)さん: 「(.*)」\s+\/ X/i);
     if ( document.URL.match(/\/status\//) && match ) {
-            const userid = document.querySelector('div[data-testid="User-Name"] > div:nth-child(2) > div > div > a > div > span').innerText;
-            document.title = `${match[1]} (${userid}): ${match[2]}`;
-            clearInterval(intervalID);
+            const useridEl = document.querySelector('div[data-testid="User-Name"] > div:nth-child(2) > div > div > a > div > span')
+            if (useridEl) {
+                const userid = useridEl.innerText;
+                document.title = `${match[1]} (${userid}): ${match[2]}`;
+                clearInterval(intervalID);
+            }
     }
 }, 1000);
