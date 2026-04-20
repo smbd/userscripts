797fe2a202b6d5e4f9046bc7e3505ae55b85215e
Mitsuru SHIMAMURA <smbd.jp@gmail.com>
2026-04-20 20:57:38 +0900

    fix geo link

--- wikipedia-googlemap.user.js
+++ wikipedia-googlemap.user.js
@@ -3,7 +3,7 @@
 // @description  Wikipedia link location to Google map
 // @namespace    http://smbd.jp
 // @author       smbd
-// @version      2024-08-13-01
+// @version      1.1
 // @match        https://*.wikipedia.org/wiki/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikipedia.org
 // @grant        none
@@ -16,13 +16,14 @@
 
     const GOOGLEMAP_URL = "https://maps.google.com/maps?hl=ja&t=m&z=12&ll=";
 
-    const GEOHACK_HOSTNAME = "geohack.toolforge.org"
+    const GEOHACK_HOSTNAME = "geohack.toolforge.org";
     const GEOHACK_SELECTOR = 'a[href*="//' + GEOHACK_HOSTNAME + '/"]';
 
     document.querySelectorAll(GEOHACK_SELECTOR).forEach(e => {
-        let latitude, longitude
-        [ latitude, longitude ] = e.querySelector("a:has(span.geo-default) > span.geo-nondefault > span > span.geo").innerText.replace(';','').split(' ');
-        let ll = latitude + "," + longitude
-        e.href = GOOGLEMAP_URL + ll + '&q=' + ll
+        let latitude, longitude;
+        [ latitude, longitude ] = e.querySelector("span > span.geo-dec + span > span.geo").innerText.replace(';','').split(' ');
+        let ll = latitude + "," + longitude;
+        e.href = GOOGLEMAP_URL + ll + '&q=' + ll;
+        e.target = "_blank";
     });
 })();
